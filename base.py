d03a405b94ca107fb4e0382203b9b464599e8209
Eugenia Berrino <eugeniaberrino@Hender-MacBook-Pro.local>
2024-06-17 22:03:57 -0300

    fix

--- base.py
+++ base.py
@@ -32,18 +32,3 @@ def get_resource_from_hapi_fhir(resource_id, resource_type):
         print(f"Error al obtener el recurso: {response.status_code}")
         print(response.json())
 
-if __name__ == "__main__":
-    # Parámetros del paciente (se puede dejar algunos vacíos)
-    family_name = "Doe"
-    given_name = "John"
-    birth_date = "1990-01-01"
-    gender = "male"
-    phone = None 
-
-    # Crear y enviar el recurso de paciente
-    patient = create_patient_resource(family_name, given_name, birth_date, gender, phone)
-    patient_id = send_resource_to_hapi_fhir(patient, 'Patient')
-
-    # Ver el recurso de paciente creado
-    if patient_id:
-        get_resource_from_hapi_fhir(patient_id,'Patient')
